132c29ca3d98db30b8d42732bdaacf4bafcb2463
IvanFadieiev <ivanfadeev91@gmail.com>
2017-02-03 14:24:15 +0200

    devise

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,4 +2,12 @@ module PostsHelper
   def selected_category
     @category ? @category : @post.category
   end
+
+  def check_link(link)
+    if link.include?('http://') || link.include?('https://')
+      link
+    else
+      'http://' + link
+    end
+  end
 end
